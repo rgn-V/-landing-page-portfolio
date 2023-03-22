d5ec8b4a14f17c0b6e81b00c42b4906341dd700b
rgn-V <nlo.nik@mail.ru>
2023-03-23 01:14:35 +0300

    added switching between work blocks

--- index.js
+++ index.js
@@ -14,6 +14,34 @@ const handleClick = (e) => {
         case 'Contact':
             window.scrollTo({ block: "start", inline: "nearest", top: document.querySelector('.page__contact').offsetTop, behavior: 'smooth' });
             break;
+    };
+
+    if (e.target.dataset.name === 'All' || e.target.dataset.name === 'UI' || e.target.dataset.name === 'UX' || e.target.dataset.name === 'Design') {
+        document.querySelector('.button_active').classList.remove('button_active')
+        e.target.classList.add('button_active')
+
+        const all = document.querySelectorAll('.items-works__item')
+
+        switch (e.target.dataset.name) {
+            case 'All':
+                all.forEach((el) => el.style.display = 'block')
+                break;
+            case 'UI':
+                const el1 = document.querySelector('[data-work-type="ui"]');
+                all.forEach((el) => el.style.display = 'none')
+                el1.style.display = 'block'
+                break;
+            case 'UX':
+                const el2 = document.querySelector('[data-work-type="ux"]');
+                all.forEach((el) => el.style.display = 'none')
+                el2.style.display = 'block'
+                break;
+            case 'Design':
+                const el3 = document.querySelector('[data-work-type="design"]');
+                all.forEach((el) => el.style.display = 'none')
+                el3.style.display = 'block'
+                break;
+        };
     }
 }
 
